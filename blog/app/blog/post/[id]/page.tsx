61c816df7a380f53b527a6530236be75a521b7f5
Shafa Naura Priesha Ardhynindya <shafanaura48@gmail.com>
2025-02-10 11:43:00 +0700

    fix: param

--- blog/app/blog/post/[id]/page.tsx
+++ blog/app/blog/post/[id]/page.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
 
-export default function Post({ params: { id } }) {
+export default async function Post({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
   return (
     <div>
       <h3>Post #{id}</h3>
